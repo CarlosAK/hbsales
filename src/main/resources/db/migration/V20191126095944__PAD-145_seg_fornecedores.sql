8edfb7b4cee09890cf1d83fe7e498ac19e3a6a74
CarlosAK <carlos.koser8@gmail.com>
2019-11-28 09:16:06 -0300

    atividade 2

---
+++ src/main/resources/db/migration/V20191126095944__PAD-145_seg_fornecedores.sql
@@ -0,0 +1,14 @@
+create table seg_fornecedores
+(
+    id  BIGINT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+    razao_social VARCHAR (100) NOT NULL,
+    cnpj VARCHAR (50)  NOT NULL,
+    nome VARCHAR (255) NOT NULL,
+    endereco VARCHAR (255) NOT NULL,
+    telefone VARCHAR (36) NOT NULL,
+    email VARCHAR (255) NOT NULL,
+
+);
+
+    create unique index ix_seg_fornecedores_01 on seg_fornecedores (razao_social asc);
+    create unique index ix_seg_fornecedores_02 on seg_fornecedores (cnpj asc);
---
+++ src/main/resources/db/migration/V20191126160429__PAD-145_seg_categorias.sql
@@ -0,0 +1,9 @@
+create table seg_categorias
+(
+    id    BIGINT IDENTITY (1, 1) NOT NULL,
+    nome VARCHAR(100)           NOT NULL,
+    id_fornecedor BIGINT         NOT NULL,
+    CONSTRAINT fk_produto_fornecedor FOREIGN KEY (id_fornecedor)
+    REFERENCES seg_fornecedores (id)
+
+);
